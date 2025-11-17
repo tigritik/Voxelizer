b6076c5165f202a45d9467c6b7bb1825b05235f4
tigritik <tigritik@gmail.com>
2025-11-16 20:26:54 -0500

    voxel filling algorithm

--- src/main.py
+++ src/main.py
@@ -57,9 +57,44 @@ def setup_voxels(size, center=(0,0,0), voxels_per_side=10):
     voxels_out["z"] = grid[:, 2].reshape((1, 1, voxels_per_side))
     return voxels_out
 
-print(load_images(imgs))
-print(get_silhouettes(imgs))
+def nearest_neighbor(x, y):
+    return int(np.rint(x)), int(np.rint(y))
+
+def project_voxel(voxel, P, interp=nearest_neighbor):
+    v = np.array([voxel["x"], voxel["y"], voxel["z"], 1])
+    proj = P @ v
+    return interp(proj[0]/proj[2], proj[1]/proj[2])
+
+def construct_model(voxels, silhouettes, projections):
+    assert len(silhouettes) == len(projections), "Unequal number of silhouettes and projections!"
+    n = len(silhouettes)
+    count = 0 # count number of voxels that are occupied
+    for i, j, k in np.ndindex(voxels.shape):
+        if i % 10 == 0: print(f"{i+1}/100")
+        voxel = voxels[i, j, k]
+        images_matched = 0
+        for sil, P in zip(silhouettes, projections):
+            pix_x, pix_y = project_voxel(voxel, P)
+            try:
+                if np.array_equal(sil[pix_y, pix_x], [255, 255, 255]):
+                    images_matched += 1
+                else:
+                    break
+            except IndexError:
+                break
+        if images_matched == n:
+            #voxels[i, j, k][["r", "g", "b"]] = (255, 255, 255)
+            voxels[i, j, k]["r"] = 255
+            voxels[i, j, k]["g"] = 255
+            voxels[i, j, k]["b"] = 255
+            count += 1
+
+    return count
+
+sil = get_silhouettes(imgs)
 np.set_printoptions(suppress=True)
-print(get_projections(imgs))
-print(setup_voxels((5,5,5))[0, 1, 2])
+proj = get_projections(imgs)
+v = setup_voxels((5,5,5), voxels_per_side=10)
+construct_model(v, sil, proj)
+print(v)
 
